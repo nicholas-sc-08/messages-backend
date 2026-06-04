6364b172d76aacf52ef25c24cc9231088ae04b58
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-06-04 20:34:44 -0300

    wip: controllers

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+    @Get()
+    listMessages() {
+
+    }
+
+    @Post()
+    createMessage(@Body() body: any) {
+        console.log(body);
+        
+    }
+
+    @Get("/:id")
+    getMessage(@Param("id") id: string) {
+        console.log(id);
+        
+    }
+}
