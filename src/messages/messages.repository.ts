d2e7c45b5225baa894ea4f192b6f5a4c4bcc7ac2
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-06-04 21:25:28 -0300

    feat: create message method repo

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -15,7 +15,14 @@ export class MessagesRepository {
         return messages;
     }
 
-    async create(message: string) {
+    async create(content: string) {
+        const contents = await readFile("messages.json", "utf8");
+        const messages = JSON.parse(contents);
 
+        const id = Math.floor(Math.random() * 999);
+
+        messages[id] = { id, content };
+
+        await writeFile("messages.json", JSON.stringify(messages));
     }
 }
